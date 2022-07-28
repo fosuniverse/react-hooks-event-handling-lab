a2a7f281ba2e5ab745463eb2cf7a88889eb1cb4a
Amen Atikpo <alaskale2024@gmail.com>
2022-07-28 16:24:10 +0000

    To be updated

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import  React from 'react';
+import  ReactDOM  from 'react-dom';
+
+function EyesOnMe(){
+    let eyesOnMe = 'blur';
+    function handleClick(event){
+        eyesOnMe =! eyesOnMe;
+        console.log(eyesOnMe)
+    }
+    return(
+        <div>
+            <h2>Eyes on me</h2>
+           <button onClick ={handleClick}>  {eyesOnMe ? 'Good!':'Hey! Eyes on me!'} </button> 
+        </div>
+    )
+}
+
+ export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
+import  React from 'react';
+import  ReactDOM  from 'react-dom';
 
 function Keypad (){
+    function handleChange(event){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                placeholder="Enter Password"
+                onChange={handleChange}
+            />
+        </div>
     )
 }
 
